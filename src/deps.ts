5e74f5a31a1e12461f751532eb7eb81608cce89d
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-18 16:59:03 +0100

    reorg

--- src/deps.ts
+++ src/deps.ts
@@ -2,7 +2,10 @@
 export { hash, uint256 } from "https://esm.sh/starknet@5.24.3";
 export type {
   BlockHeader,
+  Event,
   EventWithTransaction,
+  Transaction,
+  TransactionReceipt,
 } from "https://esm.sh/@apibara/indexer@0.2.2/starknet";
 
 // Ethereum
@@ -12,12 +15,14 @@ export {
   isAccessListEIP2930Tx,
   isFeeMarketEIP1559TxData,
   isLegacyTx,
-} from "https://esm.sh/@ethereumjs/tx@5.1.0";
-export type {
-  Capability,
-  JsonRpcTx,
   LegacyTransaction,
   TransactionFactory,
+} from "https://esm.sh/@ethereumjs/tx@5.1.0";
+
+export type {
+  JsonRpcTx,
+  TypedTransaction,
+  TypedTxData,
 } from "https://esm.sh/@ethereumjs/tx@5.1.0";
 
 export {
@@ -27,7 +32,3 @@ export {
   concatBytes,
   intToHex,
 } from "https://esm.sh/@ethereumjs/util@9.0.1";
-
-export type { AccessListBytes } from "https://esm.sh/@ethereumjs/common@4.1.0";
-
-// Other
--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,17 +1,97 @@
-import { JsonRpcTx } from "../deps.ts";
+// Starknet
+import {
+  BlockHeader,
+  Transaction,
+  TransactionReceipt,
+  uint256,
+} from "../deps.ts";
+
+// Eth
 import {
   AccessListEIP2930Transaction,
+  bigIntToBytes,
   bytesToHex,
+  concatBytes,
   FeeMarketEIP1559Transaction,
   intToHex,
   isAccessListEIP2930Tx,
   isFeeMarketEIP1559TxData,
   isLegacyTx,
+  JsonRpcTx,
   LegacyTransaction,
   TransactionFactory,
   TypedTransaction,
   TypedTxData,
 } from "../deps.ts";
+
+/**
+ * @param transaction A Kakarot transaction.
+ * @param header The block header of the block containing the transaction.
+ * @param receipt The transaction receipt of the transaction.
+ * @returns The transaction in the Ethereum format, or null if the transaction is invalid.
+ * @throws Error if any function throws a non-Error.
+ */
+export function toEthTx(
+  { transaction, header, receipt }: {
+    transaction: Transaction;
+    header: BlockHeader;
+    receipt: TransactionReceipt;
+  },
+): JsonRpcTx | null {
+  const calldata = transaction.invokeV1?.calldata;
+  if (!calldata) {
+    console.error("No calldata");
+    return null;
+  }
+  const callArrayLen = BigInt(calldata[0]);
+  if (callArrayLen !== 1n) {
+    console.error(`Invalid call array length ${callArrayLen}`);
+    return null;
+  }
+
+  // callArrayLen <- calldata[0]
+  // to <- calldata[1]
+  // selector <- calldata[2];
+  // dataOffset <- calldata[3]
+  // dataLength <- calldata[4]
+  // calldataLen <- calldata[5]
+  const bytes = concatBytes(
+    ...calldata
+      .slice(6)
+      .map((x) => bigIntToBytes(BigInt(x))),
+  );
+
+  const signature = transaction.meta.signature;
+  if (signature.length !== 5) {
+    console.error(`Invalid signature length ${signature.length}`);
+    return null;
+  }
+  const r = uint256.uint256ToBN({ high: signature[1], low: signature[0] });
+  const s = uint256.uint256ToBN({ high: signature[3], low: signature[2] });
+  const v = BigInt(signature[4]);
+
+  try {
+    const ethTxUnsigned = TransactionFactory.fromSerializedData(bytes, {
+      freeze: false,
+    });
+    const ethTx = addSignature(ethTxUnsigned, r, s, v);
+
+    const blockHash = header.blockHash;
+    const blockNumber = header.blockNumber;
+    const index = receipt.transactionIndex;
+
+    return toJsonRpcTx(ethTx, blockHash, blockNumber, index);
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(`Invalid transaction: ${e.message}`);
+    } else {
+      console.error(`Unknown throw ${e}`);
+      throw e;
+    }
+    // TODO: Ping alert webhooks
+    return null;
+  }
+}
 
 /**
  * @param tx Typed transaction to be signed.
@@ -22,7 +102,7 @@ import {
  * @throws Error if the transaction is a BlobEIP4844Tx or if v param is < 35 for a
  *         LegacyTx.
  */
-export function addSignature(
+function addSignature(
   tx: TypedTransaction,
   r: bigint,
   s: bigint,
@@ -94,7 +174,7 @@ export function addSignature(
  *
  * Acknowledgement: Code taken from <https://github.com/ethereumjs/ethereumjs-monorepo>
  */
-export function toJsonRpcTx(
+function toJsonRpcTx(
   tx: TypedTransaction,
   blockHash: string | null,
   blockNumber: string | null,
