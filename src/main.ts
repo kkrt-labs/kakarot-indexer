1371b068dc5f12c1137fd81bb33cec02573b3607
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-22 14:42:32 +0100

    await on promises resolving

--- src/main.ts
+++ src/main.ts
@@ -48,59 +48,61 @@ export default async function transform({
 
   const store: Array<StoreItem> = [];
 
-  (events ?? []).forEach(async ({ transaction, receipt, event }, i) => {
-    const typedEthTx = toTypedEthTx({ transaction });
-    // Can be null if:
-    // 1. The transaction is missing calldata.
-    // 2. The transaction is a multi-call.
-    // 3. The length of the signature array is different from 5.
-    // 4. The chain id is not encoded in the v param of the signature for a
-    //    Legacy transaction.
-    // 5. The deserialization of the transaction fails.
-    if (typedEthTx === null) {
-      return null;
-    }
-    const ethTx = toEthTx({ transaction: typedEthTx, header, receipt });
-    // Can be null if:
-    // 1. The typed transaction if missing a signature param (v, r, s).
-    if (ethTx === null) {
-      return null;
-    }
+  await Promise.all(
+    (events ?? []).map(async ({ transaction, receipt, event }, i) => {
+      const typedEthTx = toTypedEthTx({ transaction });
+      // Can be null if:
+      // 1. The transaction is missing calldata.
+      // 2. The transaction is a multi-call.
+      // 3. The length of the signature array is different from 5.
+      // 4. The chain id is not encoded in the v param of the signature for a
+      //    Legacy transaction.
+      // 5. The deserialization of the transaction fails.
+      if (typedEthTx === null) {
+        return null;
+      }
+      const ethTx = toEthTx({ transaction: typedEthTx, header, receipt });
+      // Can be null if:
+      // 1. The typed transaction if missing a signature param (v, r, s).
+      if (ethTx === null) {
+        return null;
+      }
 
-    // Can be null if:
-    // 1. The event is part of the defined ignored events (see IGNORED_KEYS).
-    // 2. The event has an invalid number of keys.
-    const ethLogs = receipt.events.map((e) => {
-      return toEthLog({ transaction: ethTx, event: e });
-    }).filter((e) => e !== null) as JsonRpcLog[];
+      // Can be null if:
+      // 1. The event is part of the defined ignored events (see IGNORED_KEYS).
+      // 2. The event has an invalid number of keys.
+      const ethLogs = receipt.events.map((e) => {
+        return toEthLog({ transaction: ethTx, event: e });
+      }).filter((e) => e !== null) as JsonRpcLog[];
 
-    const ethReceipt = toEthReceipt({
-      transaction: ethTx,
-      logs: ethLogs,
-      event,
-      cumulativeGasUsed,
-    });
+      const ethReceipt = toEthReceipt({
+        transaction: ethTx,
+        logs: ethLogs,
+        event,
+        cumulativeGasUsed,
+      });
 
-    // Add the transaction to the transaction trie.
-    await transactionTrie.put(RLP.encode(i), typedEthTx.serialize());
-    // Add the receipt to the receipt trie.
-    const encodedReceipt = encodeReceipt(
-      fromJsonRpcReceipt(ethReceipt),
-      typedEthTx.type,
-    );
-    await receiptTrie.put(RLP.encode(i), encodedReceipt);
-    // Add the logs bloom of the receipt to the block logs bloom.
-    const receiptBloom = new Bloom(hexToBytes(ethReceipt.logsBloom));
-    blockLogsBloom.or(receiptBloom);
-    cumulativeGasUsed += BigInt(ethReceipt.gasUsed);
+      // Add the transaction to the transaction trie.
+      await transactionTrie.put(RLP.encode(i), typedEthTx.serialize());
+      // Add the receipt to the receipt trie.
+      const encodedReceipt = encodeReceipt(
+        fromJsonRpcReceipt(ethReceipt),
+        typedEthTx.type,
+      );
+      await receiptTrie.put(RLP.encode(i), encodedReceipt);
+      // Add the logs bloom of the receipt to the block logs bloom.
+      const receiptBloom = new Bloom(hexToBytes(ethReceipt.logsBloom));
+      blockLogsBloom.or(receiptBloom);
+      cumulativeGasUsed += BigInt(ethReceipt.gasUsed);
 
-    // Add all the eth data to the store.
-    store.push({ collection: "transactions", data: { tx: ethTx } });
-    store.push({ collection: "receipts", data: { receipt: ethReceipt } });
-    ethLogs.forEach((ethLog) => {
-      store.push({ collection: "logs", data: { log: ethLog } });
-    });
-  });
+      // Add all the eth data to the store.
+      store.push({ collection: "transactions", data: { tx: ethTx } });
+      store.push({ collection: "receipts", data: { receipt: ethReceipt } });
+      ethLogs.forEach((ethLog) => {
+        store.push({ collection: "logs", data: { log: ethLog } });
+      });
+    }),
+  );
 
   const ethHeader = await toEthHeader({
     header: header,
