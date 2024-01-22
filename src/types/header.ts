918023317baa7425bdd3ef5b5fbbbece2b708c71
Gregory Edison <gregory.edison1993@gmail.com>
2024-01-22 14:01:19 +0100

    block header

---
+++ src/types/header.ts
@@ -0,0 +1,43 @@
+// Starknet
+import { BlockHeader } from "../deps.ts";
+
+// Eth
+import { bigIntToHex, Bloom, bytesToHex, JsonHeader } from "../deps.ts";
+
+const BLOCK_GAS_LIMIT = 30_000_000n;
+const BASE_FEE_PER_GAS = 100_000_000_000n;
+
+/**
+ * @param header - A Starknet block header.
+ * @param receipts - The receipts of the transactions in the block in json RPC format.
+ * @param transactions - The transactions in the block in a typed transaction format.
+ * @returns The Ethereum block header.
+ */
+export function toEthHeader(
+  { header, gasUsed, logsBloom, receiptRoot, transactionRoot }: {
+    header: BlockHeader;
+    gasUsed: bigint;
+    logsBloom: Bloom;
+    receiptRoot: Uint8Array;
+    transactionRoot: Uint8Array;
+  },
+): JsonHeader {
+  return {
+    parentHash: header.parentBlockHash,
+    uncleHash: "0x",
+    coinbase: header.sequencerAddress,
+    stateRoot: header.newRoot,
+    transactionsTrie: bytesToHex(transactionRoot),
+    receiptTrie: bytesToHex(receiptRoot),
+    logsBloom: bytesToHex(logsBloom.bitvector),
+    difficulty: "0x0",
+    number: header.blockNumber,
+    gasLimit: bigIntToHex(BLOCK_GAS_LIMIT),
+    gasUsed: bigIntToHex(gasUsed),
+    timestamp: header.timestamp,
+    extraData: "0x",
+    mixHash: "0x",
+    nonce: undefined,
+    baseFeePerGas: bigIntToHex(BASE_FEE_PER_GAS), // TBD
+  };
+}
