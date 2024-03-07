36f0210e5440004d6c19ccff8e690f0afd404bf4
Elias Tazartes <elias.tazartes@gmail.com>
2024-03-07 16:01:26 +0100

    change kakarot var name

--- src/provider.ts
+++ src/provider.ts
@@ -5,17 +5,17 @@ if (RPC_URL === undefined) {
   throw new Error("ENV: STARKNET_RPC_URL is not set");
 }
 
-const KAKAROT_CONTRACT_ADDRESS = Deno.env.get("KAKAROT_CONTRACT_ADDRESS");
-if (KAKAROT_CONTRACT_ADDRESS === undefined) {
-  throw new Error("ENV: KAKAROT_CONTRACT_ADDRESS is not set");
+const KAKAROT_ADDRESS = Deno.env.get("KAKAROT_ADDRESS");
+if (KAKAROT_ADDRESS === undefined) {
+  throw new Error("ENV: KAKAROT_ADDRESS is not set");
 }
 
 export const PROVIDER = new RpcProvider({
   nodeUrl: RPC_URL,
 });
 
-const { abi } = await PROVIDER.getClassByHash(KAKAROT_CONTRACT_ADDRESS);
+const { abi } = await PROVIDER.getClassByHash(KAKAROT_ADDRESS);
 if (abi === undefined) {
   throw new Error("Provider Error: Cannot get ABI of Kakarot contract");
 }
-export const KAKAROT = new Contract(abi, KAKAROT_CONTRACT_ADDRESS, PROVIDER);
+export const KAKAROT = new Contract(abi, KAKAROT_ADDRESS, PROVIDER);
